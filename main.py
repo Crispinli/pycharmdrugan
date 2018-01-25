f3f0598d1e8453337310e6539292c57de6ab8132
lwp <1042234868@qq.com>
2018-01-25 17:07:24 +0800

    一次训练进行100个epoch，pool_size为100

--- main.py
+++ main.py
@@ -19,8 +19,8 @@
         c. 判别器损失的计算方式不变，在生成器损失中加入 cycle loss 项
     （5）模型训练策略：
         a. 最优化算法采用 tf.train.AdamOptimizer 算法
-        b. 一次训练会进行 20 个 epoch，每个 epoch 中进行 1000 次迭代
-        c. 学习率 2e-4，每进行一个 epoch 的训练，学习率减少 1e-5
+        b. 一次训练会进行 100 个 epoch，每个 epoch 中进行 1000 次迭代
+        c. 学习率 2e-4，每进行一个 epoch 的训练，学习率减少 2e-6
         d. 训练中每个 epoch 都会打乱输入 A 和输入 B 的对应顺序
 '''
 import numpy as np
@@ -39,8 +39,8 @@ log_dir = "./output/log"  # 可视化日志路径
 ckpt_dir = "./output/checkpoint"  # 检查点路径
 
 max_images = 1000  # 数组中最多存储的训练/测试数据（batch_size, img_height, img_width, img_layer）数目
-pool_size = 50  # 用于更新D的假图像的批次数
-max_epoch = 20  # 每次训练的epoch数目
+pool_size = 100  # 用于更新D的假图像的批次数
+max_epoch = 100  # 每次训练的epoch数目
 n_critic = 1  # 判别器训练的次数
 
 img_height = 256  # 图像高度
@@ -226,7 +226,7 @@ class DRUGAN():
             for epoch in range(0, max_epoch):
                 print("In the epoch ", epoch)
                 # 每进行一个 epoch 则调整一次学习率
-                curr_lr = 2e-4 - epoch * 1e-5
+                curr_lr = 2e-4 - epoch * 2e-6
                 # 打乱输入 A 与输入 B 的对应顺序
                 random.shuffle(A_input)
                 random.shuffle(B_input)
