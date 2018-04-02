3f65cc1700176595e888facc5279116d385761cb
lwp <1042234868@qq.com>
2018-04-02 14:06:26 +0800

    数据增强过程中调整尺寸时采用双线性插值

--- arguement.py
+++ arguement.py
@@ -1,48 +1,56 @@
 from PIL import Image
 import os
+import numpy as np
+import tensorflow as tf
+from scipy.misc import imsave
 
-root_A = "./input/monet2photo/trainA"
-root_B = "./input/monet2photo/trainB"
+resize = tf.image.resize_images
+
+root_A = "./input/horse2zebra/trainA"
+root_B = "./input/horse2zebra/trainB"
 
 filenames_A = os.listdir(root_A)
 filenames_B = os.listdir(root_B)
 
-count = 0
-region = (42, 42, 214, 214)
 
-# for name in filenames_A:
-#     path_A = os.path.join(root_A, name)
-#     img = Image.open(path_A).resize([256, 256])
-#     # img_1 = img.rotate(45).crop(region).resize(img.size)
-#     # img_2 = img.rotate(-45).crop(region).resize(img.size)
-#     img_1 = img.rotate(45).resize(img.size)
-#     img_2 = img.rotate(-45).resize(img.size)
-#     img_3 = img.transpose(Image.FLIP_LEFT_RIGHT)
-#     if not os.path.exists("./input/monet2photo/train_A/"):
-#         os.makedirs("./input/monet2photo/train_A/")
-#     img.save("./input/monet2photo/train_A/" + str(count) + "_0.jpg")
-#     img_1.save("./input/monet2photo/train_A/" + str(count) + "_1.jpg")
-#     img_2.save("./input/monet2photo/train_A/" + str(count) + "_2.jpg")
-#     img_3.save("./input/monet2photo/train_A/" + str(count) + "_3.jpg")
-#     count += 1
-#     print(count)
+with tf.Session() as sess:
+    count = 0
+    region = (42, 42, 214, 214)
 
-# count = 0
+    for name in filenames_A:
+        path_A = os.path.join(root_A, name)
+        try:
+            img = Image.open(path_A).resize([256, 256])
+            img_1 = sess.run(resize(np.array(img.rotate(45).crop(region)), size=[256, 256]))
+            img_2 = sess.run(resize(np.array(img.rotate(-45).crop(region)), size=[256, 256]))
+            img_3 = img.transpose(Image.FLIP_LEFT_RIGHT)
+            if not os.path.exists("./input/horse2zebra/train_A/"):
+                os.makedirs("./input/horse2zebra/train_A/")
+            img.save("./input/horse2zebra/train_A/" + str(count) + "_0.jpg")
+            imsave("./input/horse2zebra/train_A/" + str(count) + "_1.jpg", img_1)
+            imsave("./input/horse2zebra/train_A/" + str(count) + "_2.jpg", img_2)
+            img_3.save("./input/horse2zebra/train_A/" + str(count) + "_3.jpg")
+        except:
+            continue
+        count += 1
+        print(count)
 
-for name in filenames_B:
-    path_B = os.path.join(root_B, name)
-    img = Image.open(path_B).resize([256, 256])
-    # img_1 = img.rotate(45).crop(region).resize(img.size)
-    # img_2 = img.rotate(-45).crop(region).resize(img.size)
-    # img_1 = img.rotate(45).resize(img.size)
-    # img_2 = img.rotate(-45).resize(img.size)
-    # img_3 = img.transpose(Image.FLIP_LEFT_RIGHT)
-    if not os.path.exists("./input/monet2photo/train_B/"):
-        os.makedirs("./input/monet2photo/train_B/")
-    img.save("./input/monet2photo/train_B/" + str(count) + ".jpg")
-    # img.save("./input/monet2photo/train_B/" + str(count) + "_0.jpg")
-    # img_1.save("./input/monet2photo/train_B/" + str(count) + "_1.jpg")
-    # img_2.save("./input/monet2photo/train_B/" + str(count) + "_2.jpg")
-    # img_3.save("./input/monet2photo/train_B/" + str(count) + "_3.jpg")
-    count += 1
-    print(count)
+    count = 0
+
+    for name in filenames_B:
+        path_B = os.path.join(root_B, name)
+        try:
+            img = Image.open(path_B).resize([256, 256])
+            img_1 = sess.run(resize(np.array(img.rotate(45).crop(region).resize(img.size)), size=[256, 256]))
+            img_2 = sess.run(resize(np.array(img.rotate(-45).crop(region).resize(img.size)), size=[256, 256]))
+            img_3 = img.transpose(Image.FLIP_LEFT_RIGHT)
+            if not os.path.exists("./input/horse2zebra/train_B/"):
+                os.makedirs("./input/horse2zebra/train_B/")
+            img.save("./input/horse2zebra/train_B/" + str(count) + "_0.jpg")
+            imsave("./input/horse2zebra/train_B/" + str(count) + "_1.jpg", img_1)
+            imsave("./input/horse2zebra/train_B/" + str(count) + "_2.jpg", img_2)
+            img_3.save("./input/horse2zebra/train_B/" + str(count) + "_3.jpg")
+        except:
+            continue
+        count += 1
+        print(count)
