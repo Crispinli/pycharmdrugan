3830f0bab231c7769f141e2bbdea62f5abf4780d
lwp <1042234868@qq.com>
2018-05-29 22:24:17 +0800

    对图像块进行卷积操作之后再加上残差操作

--- model.py
+++ model.py
@@ -30,6 +30,22 @@ def discriminator(inputdisc, name="discriminator"):
         return o_c5
 
 
+def residual(inputres, dim, name="resnet"):
+    '''
+    residual blocks
+    :param inputres: input tensor
+    :param dim: output channels
+    :param name: operation name
+    :return: tnesor
+    '''
+    with tf.variable_scope(name):
+        out_res = tf.pad(inputres, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        out_res = conv2d(out_res, dim, 3, 3, 1, 1, "VALID", "c1")
+        out_res = tf.pad(out_res, [[0, 0], [1, 1], [1, 1], [0, 0]], "REFLECT")
+        out_res = conv2d(out_res, dim, 3, 3, 1, 1, "VALID", "c2", do_relu=False)
+        return tf.nn.relu(out_res + inputres)
+
+
 def generator(inputgen, name="generator"):
     '''
     build the generator
@@ -43,27 +59,20 @@ def generator(inputgen, name="generator"):
 
         H, W = inputgen.get_shape().as_list()[1:3]  # 图像的高和宽
         scale = 2  # 图像下采样尺度
-        num_blocks = 4  # 图像块个数
+        num_blocks = 2  # 图像块个数
         imgs = [inputgen]  # 存储不同尺寸的图像块
         conv_blocks = []  # 用于存储图像块的卷积结果
 
         for iter in range(num_blocks):
-            img_block = resize(images=inputgen, size=[H//(scale * pow(2, iter)), W//(scale * pow(2, iter))])
+            img_block = resize(images=inputgen, size=[H // (scale * pow(2, iter)), W // (scale * pow(2, iter))])
             imgs.append(img_block)
 
         for i in range(len(imgs)):
             conv_block = conv2d(imgs[i], ngf * pow(2, i), f, f, 1, 1, "SAME", "c" + str(i + 1), do_norm=False)
+            conv_block = residual(conv_block, ngf * pow(2, i), "r" + str(i + 1) + "_1")
             conv_blocks.append(conv_block)
 
-        upsample = resize(conv_blocks[4], size=conv_blocks[3].get_shape()[1:3])
-        deconv = conv2d(upsample, conv_blocks[3].get_shape()[-1], ks, ks, 1, 1, "SAME", "dc1")
-        tensor = tf.concat(values=[deconv, conv_blocks[3]], axis=3)
-
-        upsample = resize(tensor, size=conv_blocks[2].get_shape()[1:3])
-        deconv = conv2d(upsample, conv_blocks[2].get_shape()[-1], ks, ks, 1, 1, "SAME", "dc2")
-        tensor = tf.concat(values=[deconv, conv_blocks[2]], axis=3)
-
-        upsample = resize(tensor, size=conv_blocks[1].get_shape()[1:3])
+        upsample = resize(conv_blocks[2], size=conv_blocks[1].get_shape()[1:3])
         deconv = conv2d(upsample, conv_blocks[1].get_shape()[-1], ks, ks, 1, 1, "SAME", "dc3")
         tensor = tf.concat(values=[deconv, conv_blocks[1]], axis=3)
 
